d669035e45baaf0464f0bc3f76b25213d797b830
Aleksej <115586927+Gorejkoff@users.noreply.github.com>
2023-08-27 23:54:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// строгий режим
+"use strict"
+
+
+let cubeBody = document.querySelector('.cube__body');
+let rangeY = document.querySelector('#rotateY');
+let rangeX = document.querySelector('#rotateX');
+let rangeZ = document.querySelector('#rotateZ');
+
+
+function rotate() {
+   cubeBody.style.cssText = `
+   --y: ${rangeY.value}deg;
+   --x: ${rangeX.value}deg;
+   --z: ${rangeZ.value}deg;
+   `
+}
+
+
+rangeY.addEventListener('input', rotate);
+rangeX.addEventListener('input', rotate);
+rangeZ.addEventListener('input', rotate);
+
+
+
+
