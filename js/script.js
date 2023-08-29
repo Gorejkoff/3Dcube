400facb2e2a1cdaa129061f18a0c50889aafb813
Aleksej <Gorejkoff@mail.ru>
2023-08-29 18:15:11 +0300

    animate

--- js/script.js
+++ js/script.js
@@ -22,9 +22,31 @@ rangeX.addEventListener('input', rotate);
 rangeZ.addEventListener('input', rotate);
 
 
+let inputBody = document.querySelector('.input-body');
+
+function autoRotate() {
+   rangeY.value == 360 && (rangeY.value = 0);
+   rangeX.value == 0 && (rangeX.value = 360);
+   rangeZ.value == 0 && (rangeZ.value = 360);
 
 
+   rangeY.value = Number(rangeY.value) + 0.1;
+   rangeX.value = Number(rangeX.value) - 0.1;
+   rangeZ.value = Number(rangeZ.value) - 0.1;
 
+   cubeBody.style.cssText = `
+   --y: ${rangeY.value}deg;
+   --x: ${rangeX.value}deg;
+   --z: ${rangeZ.value}deg;
+   `;
+
+   setTimeout(autoRotate, 16.666)
+}
+
+/* setTimeout(autoRotate, 3000) */
+
+
+autoRotate()
 
 
 
